e5f264841c610d382b01bd02c80eff1ee9e013d5
christian <kristianquizeno@gmail.com>
2022-05-09 19:28:35 -0500

    creando repositorio mercado liebre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/404", (req, res) => {
+  res.send("Error página no encontrada");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
