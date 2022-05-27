8549727c5727c14ab304fa4ec551025d8a5b8098
christian <kristianquizeno@gmail.com>
2022-05-26 21:10:45 -0500

    make it better2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
